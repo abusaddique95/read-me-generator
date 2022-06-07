9f8611a41214dfe237afb3c40ec8a3d25ef3e79f
abusaddique95 <abusaddique95@live.com>
2022-06-07 11:57:47 +0100

    added questions for readme

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# read-me-generator
-An application to create professional readme files.
+## Project Title
+
+## Description
+
+## Installation
+
+```
+git clone "repo name"
+cd readme-generator
+npm install
+```
+
+## Usage
+
+```
+npm run start
+```
+
+## Screenshots
+
+Add a video walk through
+
+## Contact me
+
+Email me at abusaddique95@live.com
+Visit my portfolio at
--- src/index.js
+++ src/index.js
@@ -1,1 +1,48 @@
-console.log("hello from node");
+const questions [
+{
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please enter a brief description of your project",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Please enter any installation instructions",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Please enter your project usage information",
+  },
+  {
+    type: "input",
+    name: "License",
+    message: "Please select the license used for this project",
+    choices: ["Mozilla", "MIT", "Apache", "Boost", "N/A"],
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Please enter your project contribution guidelines",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "Please enter your project test instructions:",
+  },
+  {
+    type: "input",
+    name: "Github",
+    message: "Please enter your Github username",
+  },
+  {
+    type: "input",
+    name: "Email Address",
+    message: "Please enter your email address",
+  },
+]
